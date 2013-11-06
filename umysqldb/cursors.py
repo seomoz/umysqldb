7d034b55ab5ab5460e29f6668f69c05e0bc82ec8
Brad Friedman <brad@moz.com>
2013-11-06 11:35:20 -0800

    Remove the try-except block from the execute method of the Cursor class, because there is no errorhandler member of the class

--- umysqldb/cursors.py
+++ umysqldb/cursors.py
@@ -12,12 +12,7 @@ class Cursor(pymysql.cursors.Cursor):
             args = (args,)
 
         result = 0
-        try:
-            result = self._query(query, args)
-        except:
-            exc, value, tb = sys.exc_info()
-            del tb
-            self.errorhandler(self, exc, value)
+        result = self._query(query, args)
 
         self._executed = query
         return result
